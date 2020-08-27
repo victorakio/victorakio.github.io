ea11f34403d381f920c71b96c33bebfea0d3e342
Victor Akio <victorakioz@gmail.com>
2020-08-27 02:19:57 -0300

    Updates

---
+++ precache-manifest.881232619a01ec366f709bed1fe850d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b96ae3bff6b34267bbb6e8a49e3b1c",
+    "url": "/victorakio.github.io/index.html"
+  },
+  {
+    "revision": "977a49a03b25b5539707",
+    "url": "/victorakio.github.io/static/js/2.48da3874.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/victorakio.github.io/static/js/2.48da3874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b20c40ed3524ffd095",
+    "url": "/victorakio.github.io/static/js/main.5bdaeda7.chunk.js"
+  },
+  {
+    "revision": "3d90b150acf08f258016",
+    "url": "/victorakio.github.io/static/js/runtime-main.b3eeb357.js"
+  },
+  {
+    "revision": "79212d8dfc5af447d7110baf6b9b1866",
+    "url": "/victorakio.github.io/static/media/background.79212d8d.jpg"
+  },
+  {
+    "revision": "f016bbdfa60d4dc409534d69d09edb6b",
+    "url": "/victorakio.github.io/static/media/github-explorer.f016bbdf.jpg"
+  },
+  {
+    "revision": "ac609bba5273d861b9b399316762b724",
+    "url": "/victorakio.github.io/static/media/logo.ac609bba.svg"
+  }
+]);
