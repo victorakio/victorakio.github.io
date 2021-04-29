381a29a1adbf97c6b647510f6a895e65e9e6599c
Victor Akio <victorakioz@gmail.com>
2021-04-29 01:20:15 -0300

    Updates

---
+++ precache-manifest.50ad4ea89d6aa6919506b27e8f195dc8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f266fd6989b1c575500b4111e821e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93d60a3628c89f2d1d2d",
+    "url": "/static/js/2.dad034d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dad034d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeacd6ccb1e215a0ea69",
+    "url": "/static/js/main.cfc8fc6b.chunk.js"
+  },
+  {
+    "revision": "837700d463bf3328bd54",
+    "url": "/static/js/runtime-main.69f633a9.js"
+  },
+  {
+    "revision": "6fd33c8ca670df18dbf9dfe555a57ef6",
+    "url": "/static/media/akioflix.6fd33c8c.jpg"
+  },
+  {
+    "revision": "79212d8dfc5af447d7110baf6b9b1866",
+    "url": "/static/media/background.79212d8d.jpg"
+  },
+  {
+    "revision": "e585bfeb4648af3692f15e49196c43af",
+    "url": "/static/media/cps.e585bfeb.jpg"
+  },
+  {
+    "revision": "f016bbdfa60d4dc409534d69d09edb6b",
+    "url": "/static/media/github-explorer.f016bbdf.jpg"
+  },
+  {
+    "revision": "ac609bba5273d861b9b399316762b724",
+    "url": "/static/media/logo.ac609bba.svg"
+  },
+  {
+    "revision": "93bcf9d1d55a073e892382364c9db92a",
+    "url": "/static/media/spotfinder.93bcf9d1.jpg"
+  }
+]);
